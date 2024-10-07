bf00baf68016047c52250371dc399cc133932473
Ahmad Fauzan <114381743+ojan28@users.noreply.github.com>
2024-10-07 12:28:23 +0700

    Add files via upload

---
+++ database/databasesetup.go
@@ -0,0 +1,50 @@
+package database 
+
+import(
+	"fmt"
+	"log"
+	"context"
+	"time"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSet () *mongo.Client {
+
+	client, err := mongo.NewClient (options.Client.ApplyURI("mongodb://localhost:8080"))
+
+	if err!= nil {
+        log.Fatal(err)
+    }
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	
+	client.Connect(ctx)
+	if err!= nil {
+		log.Fatal(err)
+	}
+
+	client.Ping(context.TODO(), nil)
+	if err!= nil {
+		log.println("failed to connect mongodb :")
+		return nil
+	}
+
+	fmt.Println("Successfully connected to mongodb")
+	return client
+
+}
+
+	var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection{
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return collection
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection{
+	var productCollection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return productCollection
+}
+
