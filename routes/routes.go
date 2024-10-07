d164adefbbbad412995b84a314a1455af952eaae
Ahmad Fauzan <114381743+ojan28@users.noreply.github.com>
2024-10-07 12:37:33 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,15 @@
+package routes
 
+import(
+	 
+	"github.com/ojan28/ojan-ecommerce/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
+}
